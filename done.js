fcc82f22fe2444a3f37f3e68b81ad1429cb1b604
Owen Shea <owen121529@student-schoolsofwestfield.org>
2024-10-08 13:22:30 +0000

    All

--- done.js
+++ done.js
@@ -74,3 +74,169 @@ guessGame();
 // end the function
 }
 
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame() {
+
+  // generate a random integer 1-100
+var answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+console.log(answer);
+alert(":3 "+answer);
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer
+while (guess != answer) { 
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number!");
+    // if guess equals answer display "Correct!" 
+if (guess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess >= answer) alert("Too high!");
+    // or if guess is less than answer display "Too low!" 
+else if (guess <= answer) alert("Too low!");
+    // else say "Bad input"
+else alert("Bad loop ;(");
+  // end the loop
+}
+// end the function
+}
+
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame() {
+  // generate a random integer 1-100
+var answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+console.log(answer);
+alert(":3 "+answer);
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer or to "q"
+while (guess != answer && guess != "q") {
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Input a number");
+    // if guess equals "q" display "Quitter!"
+if (guess == "q") alert("Quitter!");
+    // or if guess equals answer display "Correct!" 
+else if (guess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess > answer) alert("Too High!");
+    // or if guess is less than answer display "Too low!" 
+else if (guess < answer) alert("Too Low!");
+    // or say "Bad input"
+else alert("Bad Input ;(");
+  // end the loop
+}
+// end the function
+}
+
+/* Guess Game Program by Sheaware
+ * Adds the ability to play again. 
+ */
+
+/* newGame is an outer loop giving the user an option 
+ * to play again, using a boolean variable called again.
+ */
+
+// define function newGame
+function newGame() {
+  // create a Boolean variable for again, set to true
+var again = true;
+  // loop as long as again is true 
+while (again == true) {
+    // run the guessGame function 
+guessGame();
+    // ask to play again with again set to a confirm
+again = confirm("Want to play again?");
+    // close the loop
+}
+  // thank the player for playing
+alert("Thank you for playing");
+  // close the function
+}
+
+  
+// create a function called guessGame 
+function guessGame() {
+  // create guess and set equal to 0 
+var guess = 0;
+  // create tries and set equal to 0 
+var tries = 0;
+  // create variable answer and generate a random integer 1-100
+var answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(":3 "+answer);
+  // loop while guess is not equal to answer
+while (guess != answer) {
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number");
+    // add one to tries
+tries + 1; 
+    // if guess equals answer display "Correct in [tries] tries!" 
+if (guess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess > answer) alert("Too high!");
+    // or if guess is less than answer display "Too low!" 
+else if (guess < answer) alert("Too low!");
+    // if it's none of those, say "Bad Input!"
+else alert("Bad Input!");
+    // end the loop
+}
+  // end the function
+}
+
+/* Guess Game Program */
+
+// define function newGame
+function newGame() {
+  // create a Boolean variable for again, set to true
+var again = true;
+  // run the guessGame function
+guessGame()
+  // loop as long as again is true 
+while (again == true) {
+    // ask to play again with again set to a confirm
+ask = confirm("Do you want to play again?");
+    // if again is true run the guessGame function
+if (again == true) guessGame();
+    // close the loop
+}
+  // thank the player for playing
+alert("Thank you for playing");
+  // close the function
+}
+
+// create a function called guessGame
+function guessGame() {
+  // create guess and set equal to 0 
+var guess = 0;
+  // create tries and set equal to 0 
+var tries = 0;
+  // create variable answer and generate a random integer 1-100
+var answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(":3 "+answer);
+console.log(answer);
+  // loop while guess is not equal to answer
+while (guess != answer) {
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number!");
+    // add one to tries
+tries + 1;
+    // if guess equals answer display "Correct in [tries] tries!" 
+if (guess == answer) alert("Correct!");
+    // or if guess is "q" break the loop
+else if (guess == "q" || guess == "quit" || guess == "Quit" || guess == "Q") alert("Quitter!"); 
+    // or if guess is greater than answer display "Too high!" 
+else if (guess >= answer) alert("Too High!");
+    // or if guess is less than answer display "Too low!" 
+else if (guess <= answer) alert("Too low!");
+    // if it's none of those, say "Bad Input!"
+else alert("Bad Input!");
+    // end the loop
+  }
+  // end the function
+}
--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-/* Simple Guess Program */
+/* Guess Game Program */
 
-// define function guessGame
+// define function newGame
+function newGame() {
+  // create a Boolean variable for again, set to true
+var again = true;
+  // run the guessGame function
+guessGame()
+  // loop as long as again is true 
+while (again == true) {
+    // ask to play again with again set to a confirm
+ask = confirm("Do you want to play again?");
+    // if again is true run the guessGame function
+if (again == true) guessGame();
+    // close the loop
+}
+  // thank the player for playing
+alert("Thank you for playing");
+  // close the function
+}
+
+// create a function called guessGame
 function guessGame() {
-
-  // generate a random integer 1-100
-var number = math.random()*1+100;
-  // display the integer for testing purposes only
-console.log(number);
-alert(":3 "+number);
   // create guess and set equal to 0 
 var guess = 0;
+  // create tries and set equal to 0 
+var tries = 0;
+  // create variable answer and generate a random integer 1-100
+var answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(":3 "+answer);
+console.log(answer);
   // loop while guess is not equal to answer
-while (guess != answer) { 
+while (guess != answer) {
     // set guess equal to asking user to "Guess a number, 1-100"
 guess = prompt("Guess a number!");
-    // if guess equals answer display "Correct!" 
+    // add one to tries
+tries + 1;
+    // if guess equals answer display "Correct in [tries] tries!" 
 if (guess == answer) alert("Correct!");
+    // or if guess is "q" break the loop
+else if (guess == "q" || guess == "quit" || guess == "Quit" || guess == "Q") alert("Quitter!"); 
     // or if guess is greater than answer display "Too high!" 
-if (guess >= answer) alert("Too high!");
+else if (guess >= answer) alert("Too High!");
     // or if guess is less than answer display "Too low!" 
-if (guess <= answer) alert("Too low!");
-    // else say "Bad input"
-else alert("Bad loop ;(");
-  // end the loop
-}
-// end the function
+else if (guess <= answer) alert("Too low!");
+    // if it's none of those, say "Bad Input!"
+else alert("Bad Input!");
+    // end the loop
+  }
+  // end the function
 }
